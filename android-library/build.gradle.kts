82b91d9dd063d531faed95e7fb2a23f80e2c4a04
Bryan Jacobs <b@q3q.us>
2024-02-23 21:21:12 +1100

    Remove extraneous deps

--- android-library/build.gradle.kts
+++ android-library/build.gradle.kts
@@ -16,8 +16,8 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        // minSdk = 23 FIXME
-        minSdk = 31
+        minSdk = 23
+        //minSdk = 31
         targetSdk = 33
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -39,12 +39,6 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = libs.versions.composecompiler.get()
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -54,17 +48,11 @@ android {
 }
 
 dependencies {
-    implementation(project(":ui"))
     implementation(project(":fidok"))
     implementation(libs.core.ktx)
-    implementation(libs.lifecycle.runtime.ktx)
     implementation(libs.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
-    debugImplementation(libs.ui.tooling)
-    debugImplementation(libs.ui.test.manifest)
-    implementation(libs.androidx.runtime.livedata)
 
     implementation(libs.kermit)
 }
